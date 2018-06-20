dbe553dc3ec03d433fc440359e5c743f07dd4a7f
pranav-ap <pranavap96@gmail.com>
2018-06-20 09:06:35 +0530

    basic text classifier set up

---
+++ text_classification.py
@@ -0,0 +1,41 @@
+import nltk
+import random
+from nltk.corpus import movie_reviews
+
+documents = [(set(movie_reviews.words(fileid)), category)
+             for category in movie_reviews.categories()
+             for fileid in movie_reviews.fileids(category)]
+
+random.shuffle(documents)
+
+all_words = []
+
+for w in movie_reviews.words():
+    all_words.append(w.lower())
+
+all_words = nltk.FreqDist(all_words)
+
+word_features = all_words.most_common(3000)
+
+
+def find_features(review_words):
+    features = {}
+    for w in word_features:
+        # check if word available in review_words
+        features[w] = (w in review_words)
+
+    return features
+
+
+featuresets = [(find_features(review_words), category) 
+                for (review_words, category) in documents]
+
+
+training_set = featuresets[:1900]
+test_set = featuresets[1900:]
+
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+
+accuracy = nltk.classify.accuracy(classifier, test_set) * 100
+
+
